663e2d8d0912ebd5d1d3b1177f1b4f7a09dd4cd1
k1rcocichka <123amogus@gmail.com>
2024-12-30 16:20:04 +0300

    update

--- main.py
+++ main.py
@@ -1,50 +1,69 @@
 import pygame
+import math
 
 
-screen = pygame.display.set_mode((600, 300))
-speed = 7
-player_x = 100
-player_y = 200
-is_jumping = False
-jump_height = 7
-p = pygame.image.load("i.png")
-p = pygame.transform.scale(p, (100, 200))
+class Player():
+    """класс игрока"""
+    def __init__(self, image, position):
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.image, (105, 90))
+        self.rect = self.image.get_rect()
+        self.rect.center = position
+        self.speed_x = 3
+        self.speed_y = 3
+        self.angle = 0
 
-running = True
-while running:
-    screen.fill((255, 255, 255))
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP] and (150 < player_x < 250) and player_y >= 100:
-        player_y = 50
-    if keys[pygame.K_DOWN] and (100 < player_x < 250) and player_y < 90:
-        player_y = 200
-    if keys[pygame.K_SPACE] and not is_jumping:
-        is_jumping = True
-    if is_jumping:
-        if jump_height >= -7:
-            if jump_height > 0:
-                player_y -= (jump_height ** 2) / 2
-            else:
-                player_y += (jump_height ** 2) / 2
-            jump_height -= 1
-        else:
-            is_jumping = False
-            jump_height = 7
+    def draw(self):
+        rotate_image = pygame.transform.rotate(self.image, self.angle)
+        rotate_rect = rotate_image.get_rect(center=self.rect.center)
+        screen.blit(rotate_image, rotate_rect)
 
-    if keys[pygame.K_LEFT] and player_x >= 0:
-        player_x -= speed
-    if keys[pygame.K_RIGHT] and player_x <= 550:
-        player_x += speed
-    if keys[pygame.K_UP] and player_y >= 0:
-        pass
-    if keys[pygame.K_DOWN] and player_y <= 250:
-        pass
-    screen.fill((0, 0, 0))
-    screen.blit(p, (200, 100))
-    pygame.draw.rect(screen, (255, 0, 0), (player_x, player_y, 50, 50))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.time.Clock().tick(100)
+    def move(self):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_w]:
+            self.rect.y -= self.speed_y
+        if key[pygame.K_s]:
+            self.rect.y += self.speed_y
+        if key[pygame.K_a]:
+            self.rect.x -= self.speed_x
+        if key[pygame.K_d]:
+            self.rect.x += self.speed_x
 
-    pygame.display.flip()
+    def angle_finder(self, target_pos):
+        d_x = target_pos[0] - self.rect.centerx
+        d_y = target_pos[1] - self.rect.centery
+        self.angle =- math.degrees(math.atan2(d_y, d_x))
+
+
+#конфиг
+if __name__ == '__main__':
+    FPS = 60
+    running = True
+    SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 600, 600
+    BG_COLOR = (20), (120), (0)
+    
+    player = Player(image="player.png", position=(50, 50))
+    
+    #запуск
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode(SIZE)
+    pygame.display.set_caption("cursed_wood")
+    
+    #цикл
+    while running:
+        for event in pygame.event.get():
+            if event == pygame.QUIT:
+                running = False
+    
+        screen.fill(BG_COLOR)
+
+        player.angle_finder(pygame.mouse.get_pos())
+        player.draw()
+        player.move()
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+pygame.quit()
